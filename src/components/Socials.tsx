7104f931515d846c73c463ae0ccd29b1c4dd2df1
Phillip Miles <32995520+phillipmiles@users.noreply.github.com>
2025-02-28 13:42:37 +1100

    Improved accessibility

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -16,7 +16,11 @@ const Socials = ({ className }: Props) => {
   return (
     <ul className={`${className} ${s.nav} ${s.socials}`}>
       <li>
-        <Link href="https://www.instagram.com/woodfortcases/" target="_blank">
+        <Link
+          href="https://www.instagram.com/woodfortcases/"
+          target="_blank"
+          aria-label="Woodfort Cases on Instagram"
+        >
           <FontAwesomeIcon icon={faInstagram} size="xl" />
         </Link>
       </li>
@@ -25,16 +29,28 @@ const Socials = ({ className }: Props) => {
           href="https://www.youtube.com/channel/UCpavxWMrqsFSqZqnGRO3MFQ"
           target="_blank"
         >
-          <FontAwesomeIcon icon={faYoutube} size="xl" />
+          <FontAwesomeIcon
+            icon={faYoutube}
+            size="xl"
+            aria-label="Woodfort Cases on Youtube"
+          />
         </Link>
       </li>
       <li>
         <Link href="https://x.com/WoodfortCases" target="_blank">
-          <FontAwesomeIcon icon={faTwitter} size="xl" />
+          <FontAwesomeIcon
+            icon={faTwitter}
+            size="xl"
+            aria-label="Woodfort Cases on X (formally Twitter)"
+          />
         </Link>
       </li>
       <li>
-        <Link href="https://au.pinterest.com/woodfortcases/" target="_blank">
+        <Link
+          href="https://au.pinterest.com/woodfortcases/"
+          target="_blank"
+          aria-label="Woodfort Cases on Pinterest"
+        >
           <FontAwesomeIcon icon={faPinterest} size="xl" />
         </Link>
       </li>
